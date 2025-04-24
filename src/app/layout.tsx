d4f9adc58f8ed0d9d4b161251bfffb4bf361f5e9
NaotoCoding <naoto.zaniah@gmail.com>
2025-04-24 22:34:26 +0900

    レトロなUIを導入

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,10 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { DotGothic16 } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+const dotGothic16 = DotGothic16({
+  variable: "--font-dot-gothic-16",
+  weight: ["400"],
 });
 
 export const metadata: Metadata = {
@@ -24,9 +19,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        {children}
-      </body>
+      <body className={dotGothic16.className}>{children}</body>
     </html>
   );
 }
